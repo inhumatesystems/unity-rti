00ee29c45b945463e85cb602c14a8302d50f582d
Martin Insulander <martin.insulander@inhumatesystems.com>
2024-03-26 15:38:10 +0100

    Add options to RTIInjectableActivator

--- Packages/com.inhumatesystems.rti/Runtime/RTIInjectableActivator.cs
+++ Packages/com.inhumatesystems.rti/Runtime/RTIInjectableActivator.cs
@@ -8,14 +8,48 @@ namespace Inhumate.Unity.RTI {
 
         public GameObject[] objects;
 
+        public enum EventMode {
+            Never,
+            Begin,
+            End,
+            Stop,
+            Cancel,
+            StopOrCancel
+        }
+
+        public EventMode activateOn = EventMode.Begin;
+        public EventMode deactivateOn = EventMode.End;
+
         public override void Begin() {
-            foreach (var o in objects) if (o != null) o.SetActive(true);
+            if (activateOn == EventMode.Begin) Activate();
+            if (deactivateOn == EventMode.Begin) Deactivate();
             base.Begin();
         }
 
         public override void End() {
-            foreach (var o in objects) if (o != null) o.SetActive(false);
+            if (activateOn == EventMode.End) Activate();
+            if (deactivateOn == EventMode.End) Deactivate();
             base.End();
+        }
+
+        public override void Stop() {
+            if (activateOn == EventMode.Stop || activateOn == EventMode.StopOrCancel) Activate();
+            if (deactivateOn == EventMode.Stop || deactivateOn == EventMode.StopOrCancel) Deactivate();
+            base.Stop();
+        }
+
+        public override void Cancel() {
+            if (activateOn == EventMode.Cancel || activateOn == EventMode.StopOrCancel) Activate();
+            if (deactivateOn == EventMode.Cancel || deactivateOn == EventMode.StopOrCancel) Deactivate();
+            base.Cancel();
+        }
+
+        private void Activate() {
+            foreach (var o in objects) if (o != null) o.SetActive(true);
+        }
+
+        private void Deactivate() {
+            foreach (var o in objects) if (o != null) o.SetActive(false);
         }
 
     }
